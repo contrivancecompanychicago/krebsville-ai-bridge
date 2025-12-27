812ccb33fb1b6752f31de67fe00a5c6f3b94fac9
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2025-12-27 13:00:47 -0600

    Add logic for handling glitched reality anchor

---
+++ DrSharma.py
@@ -0,0 +1,4 @@
+# Example logic for your Python Engine
+if remote_config['patient_001_config']['reality_anchor']['is_glitched']:
+    trigger_malice_ui()
+    start_timer(remote_config['patient_001_config']['archivist_modifiers']['horde_transformation_timer'])
